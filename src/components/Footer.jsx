a30b3d56bb8c5fdd9b08f475a294f787a537819d
TaiwoSoftware <davcodes24@gmail.com>
2023-08-16 12:31:24 +0100

    almost done with the loop studio challenge

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,25 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import logo from "./logo.svg";
+import facebook from './icon-facebook.svg';
+import twitter from './icon-twitter.svg';
+import pinterest from './icon-pinterest.svg';
+import instagram from './icon-instagram.svg';
 function Footer() {
   return (
-    <div>
       <footer>
-        <img src={logo} alt="logo" />
+        <div className="flex-logo">
+            <div>
+                <img src={logo} alt="logo" />
+            </div>
+            <div>
+                <img src={facebook} alt="facebook" />
+                <img src={twitter} alt="twitter" />
+                <img src={pinterest} alt="pinterest" />
+                <img src={instagram} alt="instagram" />
+            </div>
+        </div>
       </footer>
-    </div>
   );
 }
 
